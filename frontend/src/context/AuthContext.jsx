99c49dc8f31d14e293fe4873ceabae7782325a79
aritra sharma <98529719+aritra1189@users.noreply.github.com>
2025-02-21 00:05:17 +0530

    Update AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -10,7 +10,7 @@ export const AuthProvider = ({ children }) => {
   useEffect(() => {
     const fetchUser = async () => {
       try {
-        await axios.get("http://localhost:5000/api/auth/me", { withCredentials: true })
+        await axios.get("https://colab-433t.onrender.com/api/auth/me", { withCredentials: true })
           .then(res => setUser(res.data))
           .catch(() => setUser(null));
       } catch (error) {
@@ -22,12 +22,12 @@ export const AuthProvider = ({ children }) => {
   }, []);
 
   const login = async (email, password) => {
-    await axios.post("http://localhost:5000/api/auth/login", { email, password }, { withCredentials: true });
+    await axios.post("https://colab-433t.onrender.com/api/auth/login", { email, password }, { withCredentials: true });
     await fetchUser(); // Refresh user data after login
   };
 
   const logout = async () => {
-    await axios.post("http://localhost:5000/api/auth/logout", {}, { withCredentials: true });
+    await axios.post("https://colab-433t.onrender.com/api/auth/logout", {}, { withCredentials: true });
     setUser(null);
   };
 
